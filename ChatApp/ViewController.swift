a42720ba53510c76027afd443b91dece41801c52
Selman Kaya <selmankaya3412@gmail.com>
2025-01-06 15:20:21 +0300

    Getting Started

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Selman Kaya on 6.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChatAppTests/ChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppTests.swift
+//  ChatAppTests
+//
+//  Created by Selman Kaya on 6.01.2025.
+//
+
+import Testing
+@testable import ChatApp
+
+struct ChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
